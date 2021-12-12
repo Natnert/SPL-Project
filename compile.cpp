8ee208e7118e4a3c9268f5b1d5c1b96ac3c53c6c
Natnert <trentantrahan817@gmail.com>
2021-12-12 15:34:58 -0500

    More files

---
+++ compile.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+
+int main(){ // C++ Entry point 
+    cout << "Wow, what a good project!";
+}
---
+++ interpret.py
@@ -0,0 +1,1 @@
+print("This project totally deserves an A!")
